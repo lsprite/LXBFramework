53542f121842dcc6a891039d7cf310185563e0db
lsprite <lsprite14@gmail.com>
2016-09-13 12:08:17 +0800

    添加录音测试方法，可在程序开始时调用开启录音权限

---
+++ mp3recorderlibrary/src/main/java/com/buihha/audiorecorder/AudioTest.java
@@ -0,0 +1,67 @@
+package com.buihha.audiorecorder;
+
+//  public boolean isFirstApp() {
+//  boolean isFirstIn = false;
+//  SharedPreferences preferences = getSharedPreferences("first_pref",
+//  MODE_PRIVATE);
+//  isFirstIn = preferences.getBoolean("isFirstIn", true);
+//  Editor editor = preferences.edit();
+//  editor.putBoolean("isFirstIn", false);
+//  return isFirstIn;
+//  }
+
+//  protected void onResume() {
+//  super.onResume();
+//  if (isFirstApp()) {
+//  AudioTest.getInstance().doTest(MyTodoActivity.this);
+//  }
+//  };
+
+import android.content.Context;
+import android.os.Environment;
+import android.os.Handler;
+
+public class AudioTest {
+    private static AudioTest instance;
+    private AudioManager mAudioManager;
+    private Handler handler = new Handler() {
+    };
+
+    public static AudioTest getInstance() {
+        if (instance == null) {
+            instance = new AudioTest();
+        }
+        return instance;
+    }
+
+    public void doTest(Context context) {
+        initAudio(context);
+        startRecode();
+        handler.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // TODO Auto-generated method stub
+                cancelRecode();
+            }
+        }, 1000);
+    }
+
+    private void initAudio(Context context) {
+        // TODO Auto-generated method stub
+        String dir = Environment.getExternalStorageDirectory()
+                + "/Android/data/" + context.getPackageName() + "/files/mp3";
+        System.out.println("�����洢·��:" + dir);
+        mAudioManager = AudioManager.getInstance(dir);
+    }
+
+    public void startRecode() {
+        mAudioManager.prepareAudio();
+    }
+
+    public void cancelRecode() {
+        mAudioManager.cancel();
+        mAudioManager = null;
+    }
+
+}
