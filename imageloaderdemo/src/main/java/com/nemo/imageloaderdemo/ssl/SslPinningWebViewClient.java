f3bf4670bb36d0b04a6d3465dafd0ab00c9679ec
lsprite <lsprite14@gmail.com>
2017-02-28 17:33:34 +0800

    webview使用https方案(缺点ajax不能使用post方法，里面的date会消失)

--- imageloaderdemo/src/main/java/com/nemo/imageloaderdemo/ssl/SslPinningWebViewClient.java
+++ imageloaderdemo/src/main/java/com/nemo/imageloaderdemo/ssl/SslPinningWebViewClient.java
@@ -16,89 +16,90 @@ import android.webkit.WebViewClient;
 
 
 /**
- * Created by mennomorsink on 06/05/15.
+ * webView.setWebViewClient(new SslPinningWebViewClient(
+ * WebActivity.this) {
  */
 public class SslPinningWebViewClient extends WebViewClient {
 
-	private Context context;
+    private Context context;
 
-	public SslPinningWebViewClient(Context context) throws IOException {
-		this.context = context;
-	}
+    public SslPinningWebViewClient(Context context) throws IOException {
+        this.context = context;
+    }
 
-	@Override
-	public WebResourceResponse shouldInterceptRequest(final WebView view,
-			String url) {
-		return processRequest(Uri.parse(url));
-	}
+    @Override
+    public WebResourceResponse shouldInterceptRequest(final WebView view,
+                                                      String url) {
+        return processRequest(Uri.parse(url));
+    }
 
-	@Override
-	@TargetApi(21)
-	public WebResourceResponse shouldInterceptRequest(final WebView view,
-			WebResourceRequest interceptedRequest) {
-		return processRequest(interceptedRequest.getUrl());
-	}
+    @Override
+    @TargetApi(21)
+    public WebResourceResponse shouldInterceptRequest(final WebView view,
+                                                      WebResourceRequest interceptedRequest) {
+        return processRequest(interceptedRequest.getUrl());
+    }
 
-	private WebResourceResponse processRequest(Uri uri) {
-		try {
-			HttpsURLConnection urlConnection = HttpsUtil
-					.getHttpsURLConnection(context, uri.toString(), "GET");
-			// Setup connection
-			urlConnection.connect();
-			// Get content, contentType and encoding
-			InputStream is = urlConnection.getInputStream();
-			//
-			// Map<String, List<String>> headerFields = urlConnection
-			// .getHeaderFields();
-			// List<String> cookiesHeader = headerFields.get("Set-Cookie");
-			// if (cookiesHeader != null) {
-			// for (String cookie : cookiesHeader) {
-			// LogUtil.log("urlconnection set-cookie", cookie.toString());
-			// }
-			// }
-			// Map map = urlConnection.getHeaderFields();
-			// Set ks = map.keySet();
-			// for (Iterator iterator = ks.iterator(); iterator.hasNext();) {
-			// String key = (String) iterator.next();
-			// System.out.println(key + ":" + map.get(key));
-			// }
-			// BufferedReader in = new BufferedReader(new InputStreamReader(is,
-			// "GBK"));
-			// StringBuffer buffer = new StringBuffer();
-			// String line = "";
-			// while ((line = in.readLine()) != null) {
-			// buffer.append(line);
-			// }
-			// String str = buffer.toString();
-			// System.out.println("++++res:" + str);
-			//
-			String contentType = urlConnection.getContentType();
-			String encoding = urlConnection.getContentEncoding();
-			// If got a contentType header
-			if (contentType != null) {
-				String mimeType = contentType;
-				// Parse mime type from contenttype string
-				if (contentType.contains(";")) {
-					mimeType = contentType.split(";")[0].trim();
-				}
-				// Return the response
-				return new WebResourceResponse(mimeType, encoding, is);
-			} else {
-				if (uri.toString().contains("jquery")
-						|| uri.toString().contains("js")) {
-					return new WebResourceResponse("text/javascript", encoding,
-							is);
-				}
-				return new WebResourceResponse("", encoding, is);
-			}
+    private WebResourceResponse processRequest(Uri uri) {
+        try {
+            HttpsURLConnection urlConnection = HttpsUtil
+                    .getHttpsURLConnection(context, uri.toString(), "GET");
+            // Setup connection
+            urlConnection.connect();
+            // Get content, contentType and encoding
+            InputStream is = urlConnection.getInputStream();
+            //
+            // Map<String, List<String>> headerFields = urlConnection
+            // .getHeaderFields();
+            // List<String> cookiesHeader = headerFields.get("Set-Cookie");
+            // if (cookiesHeader != null) {
+            // for (String cookie : cookiesHeader) {
+            // LogUtil.log("urlconnection set-cookie", cookie.toString());
+            // }
+            // }
+            // Map map = urlConnection.getHeaderFields();
+            // Set ks = map.keySet();
+            // for (Iterator iterator = ks.iterator(); iterator.hasNext();) {
+            // String key = (String) iterator.next();
+            // System.out.println(key + ":" + map.get(key));
+            // }
+            // BufferedReader in = new BufferedReader(new InputStreamReader(is,
+            // "GBK"));
+            // StringBuffer buffer = new StringBuffer();
+            // String line = "";
+            // while ((line = in.readLine()) != null) {
+            // buffer.append(line);
+            // }
+            // String str = buffer.toString();
+            // System.out.println("++++res:" + str);
+            //
+            String contentType = urlConnection.getContentType();
+            String encoding = urlConnection.getContentEncoding();
+            // If got a contentType header
+            if (contentType != null) {
+                String mimeType = contentType;
+                // Parse mime type from contenttype string
+                if (contentType.contains(";")) {
+                    mimeType = contentType.split(";")[0].trim();
+                }
+                // Return the response
+                return new WebResourceResponse(mimeType, encoding, is);
+            } else {
+                if (uri.toString().contains("jquery")
+                        || uri.toString().contains("js")) {
+                    return new WebResourceResponse("text/javascript", encoding,
+                            is);
+                }
+                return new WebResourceResponse("", encoding, is);
+            }
 
-		} catch (SSLHandshakeException e) {
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        } catch (SSLHandshakeException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-		// Return empty response for this request
-		return new WebResourceResponse(null, null, null);
-	}
+        // Return empty response for this request
+        return new WebResourceResponse(null, null, null);
+    }
 }
