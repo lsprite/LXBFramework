e4b117e039d0e54020e60ea523a12c702f0a3b2e
lsprite <lsprite14@gmail.com>
2017-03-23 16:56:30 +0800

    万年历(修改回到今天)

--- imageloaderdemo/src/main/java/com/nemo/imageloaderdemo/CalendarActivity.java
+++ imageloaderdemo/src/main/java/com/nemo/imageloaderdemo/CalendarActivity.java
@@ -14,40 +14,37 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class CalendarActivity extends Activity {
-	private CalendarView calendar;
-	private Button btn;
-	private TextView tv;
+    private CalendarView calendar;
+    private Button btn;
+    private TextView tv;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_calendar);
-		calendar = (CalendarView) findViewById(R.id.calendar);
-		calendar.setOnCalendarViewListener(new CalendarView.OnCalendarViewListener() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+        calendar = (CalendarView) findViewById(R.id.calendar);
+        calendar.setOnCalendarViewListener(new CalendarView.OnCalendarViewListener() {
 
-			@Override
-			public void onCalendarItemClick(CalendarView view, Date date) {
-				// TODO Auto-generated method stub
-				// SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-				// tv.setText(format.format(date));
-				Calendar cal = Calendar.getInstance();
-				cal.setTime(date);
-				tv.setText(CalendarUtil.getCurrentDay(cal));
-			}
-		});
-		tv = (TextView) findViewById(R.id.tv);
-		btn = (Button) findViewById(R.id.btn);
-		btn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onCalendarItemClick(CalendarView view, Date date) {
+                // TODO Auto-generated method stub
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(date);
+                tv.setText(CalendarUtil.getCurrentDay(cal));
+            }
+        });
+        tv = (TextView) findViewById(R.id.tv);
+        btn = (Button) findViewById(R.id.btn);
+        btn.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				// SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-				// tv.setText(format.format(date));
-				Calendar cal = Calendar.getInstance();
-				tv.setText(CalendarUtil.getCurrentDay(cal));
-			}
-		});
-	}
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                calendar.toToday();
+                Calendar cal = Calendar.getInstance();
+                tv.setText(CalendarUtil.getCurrentDay(cal));
+            }
+        });
+    }
 }
